90fbcc872d004fff12dbb94eb53ec0dcdc4045fc
Kartik Sharma <kartik.sharma@myntra.com>
2020-04-26 15:15:36 +0530

    Don't obey robots.txt rules :>

--- hltv_scraping/settings.py
+++ hltv_scraping/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'hltv_scraping.spiders'
 #USER_AGENT = 'hltv_scraping (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
